50f56f94aa0464b0c39be92cefd654a664f21bc0
will54-dev <will.haggard@ucas-edn.net>
2026-01-29 10:51:29 -0700

    work to gether

--- RPG Character Manager/code_rpg_character_manager.py
+++ RPG Character Manager/code_rpg_character_manager.py
@@ -1,45 +1,139 @@
-# WH, SH, JQ 2nd
+# WH, SW, JQ, BH 2nd
 
 from will_code_rpg import character_creator
+from compare_and_search import compare
+from compare_and_search import search_char
+from skills_lvs import edit_skills
+from skills_lvs import edit_level
+'''from perfun import insure
+from perfun import ensure
+from perfun import equip
+from perfun import view
+from perfun import plus 
+from perfun import minus
+from perfun import search_dict
+from perfun import edit
+from perfun import choice'''
+import sys
+import time as t
+def sprint(text, delay=0.035):  
+    for char in text:  
+        sys.stdout.write(char)  
+        sys.stdout.flush()  
+        t.sleep(delay)  
+    sprint()  
+  
+skills_library = {  
+    "wizard": {  
+        1: {"arcana", "spellcasting"},  
+        3: {"ritual casting"},  
+        5: {"fireball"},  
+        7: {"counterspell"},  
+        10: {"teleport"},  
+        15: {"meteor swarm"},  
+        20: {"wish"}  
+    },  
+    "rogue": {  
+        1: {"stealth", "thieves’ tools"},  
+        3: {"sneak attack"},  
+        5: {"evasion"},  
+        7: {"uncanny dodge"},  
+        10: {"blindsense"},  
+        15: {"slippery mind"},  
+        20: {"stroke of luck"}  
+    },  
+    "fighter": {  
+        1: {"athletics", "second wind"},  
+        3: {"action surge"},  
+        5: {"extra attack"},  
+        7: {"indomitable"},  
+        10: {"leadership"},  
+        15: {"survivor"},  
+        20: {"champion"}  
+    }  
+}  
 
 characters = dict()
 
 races = {
-    "human": (1,1,1,1,1,1,1,1,1,1)
+    "Human": (1,1,1,1,1,1),
+    "Fishman": (1,2,5,6,3,1),
+    "Avian": (2,3,6,74,8,69)
+    
 }
 classes = {
-    "human": (1,1,1,1,1,1,1,1,1,1)
+    "wizard": (1,1,1,1,1,1),
+    "rogue": (1,1,1,1,1,1),
+    "fighter": (1,1,1,1,1,1)
 }
 
+weapons={ #weapons that character has
+
+}
+inventory={ #items that player has
+
+}
+spells={ #spells and their descriptions
+
+}
+
+equipment={
+    "hand_1":'',
+    "hand_2":'',
+    "armour":'',
+}
 def main_menu():
     while True:
-        match input("Do you want to (as a number).\n1: Make a character \n2: Edit a character \n3: Compare a character \n4: Exit\n").strip():
+        match input("Do you want to (as a number).\n1: Make a character \n2: Edit a character \n3: Compare a character \n4: Search for a character \n5:Exit\n").strip():
             case "1":
-                character_creator(races,classes,characters)
+                character_creator(races,classes,characters,skills_library)
             case "2":
                 ediding_menu()
             case "3":
-                pass
+                compare(characters)
             case "4":
+                search_char(characters)
+            case "5":
                 break
             case _:
-                pass
+                sprint("not a input.")
 
 def ediding_menu():
     while True:
         if characters == {}:
-            print("no characters.")
+            sprint("no characters.")
             break
         else:
-            match input("Do you want to (as a number).\n1: Edit a character \n2: Edit a character \n3: Edit a character \n4: Exit\n").strip():
+            while True:
+                count = 0
+                new_character_list = []
+                for text in characters.keys():
+                    count += 1
+                    new_character_list.append(text)
+                    sprint(f"{count}: {text}")
+                want = input("what character do you want?\n")
+                try:
+                    characters[want]
+                    character = want
+                    break
+                except:
+                    try:
+                        want = int(want)
+                        if want > 0:
+                            characters[new_character_list[want]]
+                            character = new_character_list[want]
+                            break
+                    except:
+                        sprint("not a character.")
+            match input("Do you want to (as a number).\n1: Edit a character \n2:Edit character inventory \n4: Exit\n").strip():
                 case "1":
                     pass
                 case "2":
-                    pass
+                    choice()
                 case "3":
                     pass
                 case "4":
                     break
                 case _:
-                    pass
+                    sprint("not a input.")
 main_menu()
--- RPG Character Manager/perfun.py
+++ RPG Character Manager/perfun.py
@@ -1,4 +1,12 @@
 #Dictionaries 
+import sys
+import time as t
+def sprint(text, delay=0.015):  
+    for char in text:  
+        sys.stdout.write(char)  
+        sys.stdout.flush()  
+        t.sleep(delay)  
+    sprint() 
 weapons={ #weapons that character has
 
 }
@@ -30,7 +38,7 @@ def insure():
         if f"{num}".isnumeric() and  int(num):
             return int(num)
         else:
-                print("Please enter a valid input")
+                sprint("Please enter a valid input")
 #number ensure function (make sure user input is a number(with parameters)
 def ensure(l,h):
     while True:
@@ -39,125 +47,144 @@ def ensure(l,h):
             if f"{num}".isnumeric() and  int(num) in range(l,h):
                 return int(num)
             else:
-                print("Please enter a valid input")
+                sprint("Please enter a valid input")
         else:
             if f"{num}".isnumeric():
                 return int(num)
             else:
-                print("Please enter a valid input")
+                sprint("Please enter a valid input")
 
 #epuip function for equiping equipment
 def equip(equipment,cls):
     while True:
-        print(f'Hand 1:{equipment['hand_1']}\nHand 2:{equipment['hand_2']}\nArmour:{equipment['armour']}')
-        inp=input("Would you like to edit\n1:hands\n2:armour")
+        sprint(f'Hand 1:{equipment['hand_1']}\nHand 2:{equipment['hand_2']}\nArmour:{equipment['armour']}')
+        sprint("Would you like to edit\n1:hands\n2:armour")
+        inp=input()
         match inp:
             case "1":
-                inp=input("are you\n1:equiping\n2:stowing")
+                sprint("are you\n1:equiping\n2:stowing")
+                inp=input()
                 match inp:
                     case '1':
-                        inp=input("is the item 2 handed?\n1:yes\n2:no")
+                        sprint("is the item 2 handed?\n1:yes\n2:no")
+                        inp=input()
                         match inp:
                             case '1':
-                                inp=input("what are you equiping? ")
+                                sprint("what are you equiping?")
+                                inp=input()
                                 if equipment["hand_1"] =="" and equipment["hand_2"] =="":
                                     equipment["hand_1"],equipment["hand_2"] =f"{inp}",f"{inp}"
-                                else:print("your hands ar too full")
+                                else:sprint("your hands ar too full")
                             case '2':
-                                inp=input("what are you equiping? ")
+                                sprint("what are you equiping")
+                                inp=input()
                                 if equipment["hand_1"] =="":
                                     equipment["hand_1"] =f"{inp}"
                                 elif equipment["hand_2"] =="":
                                     equipment["hand_2"] =f"{inp}"
-                                else:print("your hands ar too full")
+                                else:sprint("your hands ar too full")
+                            case _:
+                                sprint("please use a valid input")
                     case "2":
-                        inp=input(f"What are you stowing\n1:{equipment['hand_1']}\n2:{equipment['hand_2']}")
+                        sprint(f"What are you stowing\n1:{equipment['hand_1']}\n2:{equipment['hand_2']}")
+                        inp=input()
                         match inp:
                             case "1":
                                 equipment["hand_1"]=''
                             case '2':
                                 equipment["hand_2"]=''
+                            case _:
+                                sprint("please use a valid input")
+                    case _:
+                        sprint("please use a valid input")
                 return equipment
             case "2":
-                inp=input("are you\n1:equiping\n2:dequiping")
+                sprint("are you\n1:equiping\n2:dequiping")
+                inp=input()
                 match inp:
                     case "1":
-                        inp=input("How heavy is the armour?\n1:light\n2:medium\n3:Heavy")
+                        sprint("How heavy is the armour?\n1:light\n2:medium\n3:Heavy")
+                        inp=input()
                         if (cls =='rouge' and (inp=="2" or inp=='3')) or cls=='wizard':
-                            print("You do not have training in this kind of armour")
+                            sprint("You do not have training in this kind of armour")
                         else:
-                            inp=input("what armour are you equiping?")
+                            sprint("what armour are you equiping?")
+                            inp=input()
                             if equipment['armour']=='':
                                 equipment['armour']=f'{inp}'
                             else:
-                                print("you are already wearing armour")
+                                sprint("you are already wearing armour")
                     case '2':
                         equipment['armour']=''
+                    case _:
+                        sprint("please use a valid input")
                 return equipment
-#view function (prints dictionary contents name and info)
+            case _:
+                sprint("please use a valid input")
+#view function (sprints dictionary contents name and info)
 def view(dictionary,dictname):
     for key in dictionary:
         if dictname=="weapons"or"inventory":
-            print(f"{key}:{dictionary[key][0]}, value:{dictionary[key][1]}, weight:{dictionary[key][2]}")
+            sprint(f"{key}:{dictionary[key][0]}, value:{dictionary[key][1]}, weight:{dictionary[key][2]}")
         elif dictname=="spells":
-            print(f"{key}:{dictionary[key][0]}, level:{dictionary[key][1]}, casting:{dictionary[key][2]}")
+            sprint(f"{key}:{dictionary[key][0]}, level:{dictionary[key][1]}, casting:{dictionary[key][2]}")
 #Add function (asks user for Weapon name, asks user for Weapon info, adds them to a dictionary)
 def plus(dictionary,dictname):
     name=input(f"What is the name of the item you would like to add to your inventory?\n")
     info=input(f"What is information you would like to give {name}?\n")
     match dictname:
         case "weapons":
-            print(f"What is the value of {name}?")
+            sprint(f"What is the value of {name}?")
             value=insure()
-            print(f"What is the weight of {name}?")
+            sprint(f"What is the weight of {name}?")
             weight=insure()
             dictionary[name]=[info,value,weight]
-            print(f"{name} added to inventory")
+            sprint(f"{name} added to inventory")
         case "inventory":
-            print(f"What is the value of {name}?")
+            sprint(f"What is the value of {name}?")
             value=insure()
-            print(f"What is the weight of {name}?")
+            sprint(f"What is the weight of {name}?")
             weight=insure()
             dictionary[name]=[info,value,weight]
-            print(f"{name} added to inventory")
+            sprint(f"{name} added to inventory")
         case "spells":
-            print(f"What level of spell is {name}?")
+            sprint(f"What level of spell is {name}?")
             spellev=insure()
             time=input(f"What is the casting time of {name}?")
             dictionary[name]=[info,spellev,time]
-            print(f"{name} added to inventory")
+            sprint(f"{name} added to inventory")
     return dictionary
-#Remove function (print dictionary, asks user for number Weapon that they want to remove,removes them to a dictionary)
+#Remove function (sprint dictionary, asks user for number Weapon that they want to remove,removes them to a dictionary)
 def minus(dictionary):
-    print("Note: If you remove it you will have to manually add it back!")
+    sprint("Note: If you remove it you will have to manually add it back!")
     name=input(f"Which item would you like to remove from your inventory?\n")
     if name in dictionary:
         del dictionary[name]
-        print(f'{name} has been removed')
+        sprint(f'{name} has been removed')
     else:
-        print(f"{name} wasn't found in inventory")
+        sprint(f"{name} wasn't found in inventory")
     return dictionary
-#Search (ask what they want to search by (effect or name) and print any weapons that fulfil the requirements)
-def search(dictionary):
+#Search (ask what they want to search by (effect or name) and sprint any weapons that fulfil the requirements)
+def search_dict(dictionary):
     key=list(dict(dictionary).keys())
-    print("How would you like to search your inventory\n1. name\n2. feature")
+    sprint("How would you like to search your inventory\n1. name\n2. feature")
     bol=ensure(1,3)
     inp=input("What are you searching for?")
     if bol==1:
         if inp in dictionary:
-            print(f"{inp}:{dictionary[inp][0]}, value:{dictionary[inp][1]}, weight:{dictionary[inp][2]} ")
+            sprint(f"{inp}:{dictionary[inp][0]}, value:{dictionary[inp][1]}, weight:{dictionary[inp][2]} ")
         else:
-            print(f"{inp} not in inventory")
+            sprint(f"{inp} not in inventory")
     if bol==2:
         for i in range(0,len(key)):
             if f'{inp}' in dictionary[key[i]][0]:
-                print(f"{key[i]}:{dictionary[key[i]][0]}, value:{dictionary[key[i]][1]}, weight:{dictionary[key[i]][2]}")
+                sprint(f"{key[i]}:{dictionary[key[i]][0]}, value:{dictionary[key[i]][1]}, weight:{dictionary[key[i]][2]}")
             else:
                 continue
 #main function for selection how you are editing the dictionary
 def edit(dictionary,dictname):
     while True:
-        print(f"1:View your {dictname}\n2:add to {dictname}\n3:remove a {dictname}\n4:search {dictname} for specific name/attribute\n5:Exit editor\nWhich option do you want to use?")
+        sprint(f"1:View your {dictname}\n2:add to {dictname}\n3:remove a {dictname}\n4:search {dictname} for specific name/attribute\n5:Exit editor\nWhich option do you want to use?")
         inp=ensure(1,6)
         if inp==1:
             view(dictionary,dictname)
@@ -166,16 +193,20 @@ def edit(dictionary,dictname):
         elif inp==3:
             minus(dictionary)
         elif inp==4:
-            search(dictionary)
+            search_dict(dictionary)
         elif inp==5:
             break
     return dictionary
 #User input for choosing dictionary to edit
-def choice(tuple):
-    print(f"What would you like to edit?\n1:weapons\n2:inventory\n3:equipment")
-    if cls=="wizard":
-        print("4:spells")
-    inp=ensure(1,len(tuple)+1)
+def choice(character):
+    if character['class']=="wizard":
+        dictionaries=(weapons,inventory,spells) #tuple of the dictionary names
+    else:
+        dictionaries=(weapons,inventory)
+    sprint(f"What would you like to edit?\n1:weapons\n2:inventory\n3:equipment")
+    if character["class"] =="wizard":
+        sprint("4:spells")
+    inp=ensure(0,len(dictionaries)+1)
     if inp==1:
         edit(weapons,"weapons")
         return weapons
@@ -183,7 +214,7 @@ def choice(tuple):
         edit(inventory,"inventory")
         return inventory
     if inp==3:
-        equip(equipment,cls)
+        equip(equipment,character["class"])
         return equipment
     if inp==4:
         edit(spells,"spells")
